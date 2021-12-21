aeb52299375bdd17f8c2d84cf51845df17f53e1f
ryanlin <ryanlin@outlook.com>
2021-12-21 20:45:20 +0800

    fix compile

--- packages/runtime-core/src/component.ts
+++ packages/runtime-core/src/component.ts
@@ -12,7 +12,6 @@ import {
 } from '@vue/reactivity'
 import {
   ComponentPublicInstance,
-  PublicInstanceProxyHandlers,
   createDevRenderContext,
   exposePropsOnRenderContext,
   exposeSetupStateOnRenderContext,
